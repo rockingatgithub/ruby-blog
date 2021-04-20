705784f470ecd7000a7ac21c7ed58934e3010ac1
Sudhendra Singh <sudhendra.singh@gocomet.com>
2021-04-21 01:49:15 +0530

    initial setup

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  root 'articles#index'
+
+  resources :articles do
+    resources :comments
+  end
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+
+
